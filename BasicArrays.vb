78e90d893e96f1222afcbfcef17af98c01df71a1
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2019-11-21 00:19:04 +0500

    Add files via upload

---
+++ BasicArrays.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim arr(6), I As Integer
+
+        arr(0) = 200
+        arr(1) = 400
+        arr(2) = 600
+        arr(3) = 800
+        arr(4) = 1000
+        arr(5) = 1200
+        arr(6) = 1400
+
+        For I = 1 To 6
+            Console.WriteLine("arr " & I & " is " & arr(I))
+        Next
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
