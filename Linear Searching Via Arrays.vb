fef5fb90df318d10f9d09aeeda16208dcfe89ce1
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2019-11-24 14:18:42 +0500

    This program shows linear searching in arrays

---
+++ Linear Searching Via Arrays.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim NamesArray(6), SName As String
+        Dim A As Integer
+        Dim IsItFound As Boolean
+        IsItFound = False
+
+        For A = 1 To 6
+            NamesArray(A) = " "
+        Next
+
+        For A = 1 To 5
+            Console.WriteLine("Enter a name: " & A & " ")
+            SName = Console.ReadLine
+            NamesArray(A) = SName
+        Next
+
+        Console.Write("ENter a sensible name to search for: ")
+        SName = Console.ReadLine
+        For A = 1 To 5
+            If NamesArray(A) = SName Then
+                Console.WriteLine(A)
+                IsItFound = True
+            End If
+        Next
+        If IsItFound = False Then
+            Console.WriteLine("The name you entered was not found. ")
+        End If
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
