7471ecb4655a10d600ed808521da3be3bcc8354c
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2019-11-28 20:38:20 +0500

    Add files via upload

---
+++ Denary(ARRAYS).vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim binArr(8), Denary, I As Integer
+
+        Do Until Denary >= 0 And Denary <= 255
+
+            Console.Write("Enter a Denary number between 0 to 255:")
+            Denary = Console.ReadLine()
+
+        Loop
+
+        For I = 1 To 8
+            binArr(I) = 0 
+        Next
+
+        Do While Denary > 0
+            I = I + 1
+            binArr(I) = Denary Mod 2
+            Denary = Denary / 2
+        Loop
+
+        For I = 8 To 1 Step -1
+            Console.WriteLine(binArr(I))
+        Next
+
+        Console.ReadKey()
+    End Sub
+
+End Module
